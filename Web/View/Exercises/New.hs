277ce90a6301788d0cdc63218d626c031ee422f5
Nuno Ferreira <nunogcferreira@gmail.com>
2024-12-05 20:39:26 +0000

    new shows the muscle groups

--- Web/View/Exercises/New.hs
+++ Web/View/Exercises/New.hs
@@ -31,8 +31,27 @@ renderForm exercise muscleGroups =
   formFor
     exercise
     [hsx|
-    {(textField #name)}
-    {(selectField #muscleGroup muscleGroups)}
+    {textField #name}
+    {renderMuscleGroupCheckboxGroup muscleGroups}
     {submitButton}
 
 |]
+  where
+    renderMuscleGroupCheckboxGroup :: [MuscleGroup] -> Html
+    renderMuscleGroupCheckboxGroup muscleGroups =
+      [hsx|
+        <fieldset>
+          <legend>Muscle Groups</legend>
+          {forEachWithIndex muscleGroups renderMuscleGroupCheckbox}
+        </fieldset>
+      |]
+
+    renderMuscleGroupCheckbox :: (Int, MuscleGroup) -> Html
+    renderMuscleGroupCheckbox (index, muscleGroup) =
+      let checkboxId = "checkbox-muscleGroup-" <> show index
+       in [hsx|
+          <div class="form-check">
+            <input class="form-check-input" type="checkbox" id={checkboxId} value={muscleGroup.id}>
+            <label class="form-check-label" for={checkboxId}>{muscleGroup.name}</label>
+          </div>
+      |]
