7d5c8e42c468f146505658c20c986ecc73251fce
Nuno Ferreira <nunogcferreira@gmail.com>
2024-12-05 17:10:02 +0000

    fix migrations and re-introduce the many to many relationship between muscle group and exercise

--- Application/Migration/1733413140.sql
+++
@@ -1,8 +0,0 @@
-CREATE TYPE muscle_group AS ENUM ('quads', 'hamstrings', 'calves');
-ALTER TABLE exercises ADD COLUMN muscle_groups muscle_group[] DEFAULT '{}' NOT NULL;
-DROP TABLE exercises_muscle_groups;
-DROP TABLE muscle_groups;
-ALTER TABLE muscle_groups DROP CONSTRAINT muscle_groups_name_key;
-DROP INDEX muscle_groups_created_at_index;
-DROP TRIGGER update_muscle_groups_updated_at ON muscle_groups;
-
