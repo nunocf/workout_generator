bf6b2a349509fb41a7265ea2bd877036be87252c
Nuno Ferreira <nunogcferreira@gmail.com>
2024-12-10 17:37:15 +0000

    we can now use createRecord for ExerciseWithMuscleGroups

--- Application/Helper/Controller.hs
+++ Application/Helper/Controller.hs
@@ -1,9 +1,62 @@
 module Application.Helper.Controller where
 
+import Data.ByteString qualified as ByteString
 import Data.Map qualified as Map
+import Database.PostgreSQL.Simple.ToField
+import Database.PostgreSQL.Simple.Types (Query (Query))
 import Generated.Types
 import IHP.ControllerPrelude
+import IHP.ModelSupport
+import IHP.QueryBuilder
 import Web.Types
+
+instance CanCreate ExerciseWithMuscleGroups where
+  create :: (?modelContext :: ModelContext) => ExerciseWithMuscleGroups -> IO ExerciseWithMuscleGroups
+  create (ExerciseWithMuscleGroups {exercise, muscleGroups}) = do
+    createdExercise <- create exercise
+    let exercisesMuscleGroups :: [ExercisesMuscleGroup] =
+          map
+            ( \(muscleGroup) ->
+                newRecord @ExercisesMuscleGroup
+                  |> set #exerciseId (get #id createdExercise)
+                  |> set #muscleGroupId (get #id muscleGroup)
+            )
+            muscleGroups
+    exerciseMuscleGroups <- createMany exercisesMuscleGroups
+    pure
+      ( ExerciseWithMuscleGroups
+          { muscleGroups = muscleGroups,
+            exercise = exercise
+          }
+      )
+  createMany :: (?modelContext :: ModelContext) => [ExerciseWithMuscleGroups] -> IO [ExerciseWithMuscleGroups]
+  createMany [] = pure []
+  createMany models = do
+    createdExercises <- createMany (map (.exercise) models)
+    let exercisesWithMuscleGroups = zip models createdExercises
+    let exercisesMuscleGroups =
+          concatMap
+            ( \(model, createdExercise) ->
+                map
+                  ( \(muscleGroup) ->
+                      newRecord @ExercisesMuscleGroup
+                        |> set #exerciseId (get #id createdExercise)
+                        |> set #muscleGroupId (get #id muscleGroup)
+                  )
+                  model.muscleGroups
+            )
+            exercisesWithMuscleGroups
+    _ <- createMany exercisesMuscleGroups
+    pure $
+      zipWith
+        ( \model createdExercise ->
+            ExerciseWithMuscleGroups
+              { exercise = createdExercise,
+                muscleGroups = model.muscleGroups
+              }
+        )
+        models
+        createdExercises
 
 -- | an `Exercise` with all associated MuscleGroups
 fetchExerciseWithMuscleGroups :: (?modelContext :: ModelContext) => Id Exercise -> IO ExerciseWithMuscleGroups
